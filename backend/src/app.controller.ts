0a6a87fa49d396df7518c9632adfffa1eddf0b3d
joseluismunozzuta <joseluismunozzuta@gmail.com>
2023-03-23 02:25:16 -0500

    Install axios. Create a function to obtain the commits from the repo with the Github API.

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import axios from 'axios';
 
-@Controller()
+@Controller('commits')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getAllCommits() {
+    const url =
+      'https://api.github.com/repos/joseluismunozzuta/githubCommitsViewer/commits';
+    const response = await axios.get(url);
+    const commits = response.data;
+    return commits;
   }
 }
